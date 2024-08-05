55844e081837a99c2bf0eaca97774ab07eef3bd9
Mark Hafemann <markhafemann1@gmail.com>
2024-08-04 20:46:08 -0500

    refactor: Convert test function to async/await syntax

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
-function test(): void {
+async function test(): Promise<void> {
   console.log(process.env.TEST_VALUE);
+  setTimeout(() => {
+    return "hello";
+  }, 1000);
 }
 
-test();
+await test();
