83f06ebd7d5d9e50bfef8c8044775bfa8f2b1d96
Mark Hafemann <markhafemann1@gmail.com>
2024-08-04 21:59:17 -0500

    refactor: Update test function to use async/await syntax

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,18 @@
-import { cwd } from "node:process";
-import { pathToFileURL } from "node:url";
+import { cwd } from 'node:process';
+import { pathToFileURL } from 'node:url';
 
 // test function
 console.log(pathToFileURL(cwd()));
 
 // async test function
 async function test(): Promise<void> {
-  console.log(process.env.TEST_VALUE);
-  console.log("setTimeOut");
+	console.log(process.env.TEST_VALUE);
+	console.log('setTimeOut');
 
-  setTimeout(() => {
-    console.log("setTimeOut resolved");
-    return "hello";
-  }, 1000);
+	setTimeout(() => {
+		console.log('setTimeOut resolved');
+		return 'hello';
+	}, 1000);
 }
 
 // top level await
